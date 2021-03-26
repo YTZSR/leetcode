5c02cd284b8569c2731b0ec75613cfd486aad894
Troy Han <919373602@qq.com>
2021-03-26 23:52:50 +0800

    2021-3-26

---
+++ easy/83.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        temp = head
+        while temp and temp.next:
+            if temp.next.val == temp.val:
+                temp.next = temp.next.next
+                continue
+            temp = temp.next
+        
+        
+        return head
